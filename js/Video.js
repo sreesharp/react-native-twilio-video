db6f5fe8d4fa3eed71eb4eae4f73a1aa46429213
sreesharp <sreejumon@gmail.com>
2017-04-17 14:07:25 -0700

    Updated the Video module methods [WIP]

--- js/Video.js
+++ js/Video.js
@@ -7,12 +7,46 @@ import {
 
 const { RNTwilioVideo } = NativeModules;
 
-let Video = {
+const NativeAppEventEmitter = new NativeEventEmitter(RNTwilioVideo)
 
+const Video = {
+  /**
+   * Start the video call using given token and room name
+   * @Deprecated use connect method
+   */
   startCall({roomName, accessToken}) {
-    RNTwilioVideo.startCall(accessToken,roomName)
-  }
-  
+    RNTwilioVideo.startCall(accessToken, roomName);
+  },
+
+  /**
+   * Connect to given room using given token
+   */
+  connect({roomName, accessToken}) {
+    RNTwilioVideo.startCall(accessToken, roomName);
+  },
+
+  /**
+   * Disconnect from the room
+   */
+  disconnect(roomName) {
+    console.log('Not yet supported');
+    // RNTwilioVideo.disconnect(roomName);
+    // TODO: internally call the room.disconnect
+  },
+
+  /**
+   * Release the room
+   */
+  release(roomName) {
+    console.log('Not yet supported');
+    // RNTwilioVideo.releasae(roomName);
+    // TODO: internally call Video.release
+  },
+
+  setLogLevel(level) {
+    console.log('Not yet supported');
+    // RNTwilioVideo.setLogLevel(level);
+  },
 
 }
 module.exports = Video;
--- js/index.js
+++ js/index.js
@@ -1,26 +1,8 @@
 
-// import { NativeModules } from 'react-native';
-// const { RNTwilioVideo } = NativeModules;
-// export default RNTwilioVideo;
-
-
 'use strict';
 
+// native modules
 exports.Video = require('./Video');
+
+// native components
 exports.VideoView = require('./VideoView');
-
-/*
-exports.Room = require('./Room');
-exports.LocalMedia = require('./LocalMedia');
-exports.AudioTrack = require('./AudioTrack');
-exports.LocalVideoTrack = require('./LocalVideoTrack');
-exports.CameraCapturer = require('./CameraCapturer');
-exports.VideoCapturer = require('./VideoCapturer');
-exports.VideoView = require('./VideoView');
-exports.VideoConstraints = require('./VideoConstraints');
-
-*/
-
-// exports.ConnectOptions = require(./ConnectOptions)
-//ScreenCapturer
-//MediaProjectionManager
